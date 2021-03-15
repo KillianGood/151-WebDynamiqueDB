50c310245ed87595d441670a3cbf8213a40588ad
KillianGood <61836895+KillianGood@users.noreply.github.com>
2021-03-15 11:11:58 +0100

    Connect to the DB
    Adding the db fîle connection to the db fixed

--- Code/php/index.php
+++ Code/php/index.php
@@ -1,11 +1,43 @@
-<?php
-    <html>
+<?php 
+require "database.php";
+$db = new Database();
+$teachers = $db->getAllTeachers();
+
+// echo "<pre>";
+// print_r($teachers);
+// echo "</pre>";
+?>
+<!DOCTYPE html>
+<html lang="fr">
     <head>
-        <title>Page Title</title>
+        <!--
+		ETML
+		Auteur      : Killian Good
+		Date        : 15.03.2021
+		Description : 
+		-->
+        <meta charset="UTF-8">
+        <link href="style.css" rel="stylesheet" type="text/css" />
+        <title>Surnom</title>
     </head>
     <body>
-        <h1>This is a Heading</h1>
-        <p>This is a paragraph.</p>
+        <h1>Surnom des enseignants</h1>
+        <h3>Liste des enseignants</h3>
+        <table>
+            <tr>
+                <th>Nom</th>
+                <th>Surnom</th>
+                <th>Options</th>
+            <?php foreach($teachers as $teacher): ?>
+                </tr>
+                <td><?php echo $teacher["teaName"] ?></td>
+                <td><?php echo $teacher["teaNickname"] ?></td>
+                <td>Wait</td>
+            </tr>   
+        <?php endforeach; ?>    
+        </table>    
+        <footer>
+            <p>Killian Good</p>
+        </footer>
     </body>
 </html>
-?>
